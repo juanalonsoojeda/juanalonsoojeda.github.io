e900d7028b535cb531f13d0f282616506fbea60e
Juan Ojeda <juanalonsoojeda@gmail.com>
2019-05-17 00:02:39 -0700

    Small refactor

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var btnRun = document.getElementById("btnRun");
-var txtCode = document.getElementById("txtCode");
-var ifmResult = document.getElementById("iframe");
-var txtHtml = document.getElementById("txtHtml");
-var txtCss = document.getElementById("txtCss");
+(function () {
+    var btnRun = document.getElementById("btnRun");
+    var txtCode = document.getElementById("txtCode");
+    var ifmResult = document.getElementById("iframe");
+    var txtHtml = document.getElementById("txtHtml");
+    var txtCss = document.getElementById("txtCss");
 
-btnRun.addEventListener("click", function (ev) {
-    var doc = ifmResult.contentDocument;
-    doc.open();
-    doc.writeln("<html><header><style>" + txtCss.value + "</style></header><body>" + txtHtml.value + "<script>" + txtCode.value + "</script></body></html>");
-    doc.close();
-});
+    btnRun.addEventListener("click", function (ev) {
+        var doc = ifmResult.contentDocument;
+        doc.open();
+        doc.writeln("<html><header><style>" + txtCss.value + "</style></header><body>" + txtHtml.value + "<script>" + txtCode.value + "</script></body></html>");
+        doc.close();
+    });
+})();
